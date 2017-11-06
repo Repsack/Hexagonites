b66d147010db90250f1c08574c0840c967c22962
Kasper Hansen <kh170391@gmail.com>
2017-11-06 13:38:52 +0100

    Hexagonals now exist, and they can be highlighted and respond to clicks

--- Hexagonites/Hex.cs
+++ Hexagonites/Hex.cs
@@ -1,9 +1,50 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Media;
 
 namespace Hexagonites
 {
     class Hex
     {
+        public double PI = System.Math.PI;
         public Point center;
+        public PointCollection corners;
+        private double sscale;
+        public bool highlighted { get; set; }
+
+        public double scale
+        {
+            get { return sscale; }
+            set
+            {
+                sscale = value;
+                makeCorners();
+            }
+        }
+        public Hex(Point center, double scale)
+        {
+            this.center = center;
+            this.scale = scale;
+            makeCorners();
+        }
+        private void makeCorners()
+        {
+            corners = new PointCollection();
+            for (int i = 0; i < 6; i++)
+            {
+                /*
+                var angle_deg = 60 * i   + 30
+                var angle_rad = PI / 180 * angle_deg
+                    return Point(center.x + size * cos(angle_rad), center.y + size * sin(angle_rad))
+                 */
+                double deg = 60 * i + 30;
+                double rad = PI / 180 * deg;
+                Point ptoadd = new Point(center.X + scale * System.Math.Cos(rad),
+                                      center.Y + scale * System.Math.Sin(rad));
+
+                corners.Add(ptoadd);
+            }
+        }
     }
 }
--- Hexagonites/MainWindow.xaml.cs
+++ Hexagonites/MainWindow.xaml.cs
@@ -21,9 +21,67 @@ namespace Hexagonites
     /// </summary>
     public partial class MainWindow : Window
     {
+        Hex myTestHex1;
+        Hex myTestHex2;
+        Hex myTestHex3;
+        Hex curHex;
+        Polygon curPol;
         public MainWindow()
         {
             InitializeComponent();
+            double scale = 30;
+            myTestHex1 = new Hex(new Point(0,0),scale);
+            myTestHex2 = new Hex(new Point(Math.Sqrt(3)*scale,0), scale);
+            myTestHex3 = new Hex(new Point(Math.Sqrt(3) * scale / 2, (scale/2)*3), scale);
+            myPol1.Points = myTestHex1.corners;
+            myPol2.Points = myTestHex2.corners;
+            myPol3.Points = myTestHex3.corners;
+            curHex = myTestHex1;
+            curPol = myPol1;
+        }
+
+        private void highlightHex(object sender, MouseEventArgs e)
+        {
+            //double the stroke value of the currently highlighted hex            
+            curPol = (Polygon)sender;
+            curPol.StrokeThickness *= 2;
+            if (curPol.Name == "myPol1") //WROOOOOOOOONG!!!
+            {
+                curHex = myTestHex1;
+            }
+            else if(curPol.Name == "myPol2")
+            {
+                curHex = myTestHex2;
+            }
+            else
+            {
+                curHex = myTestHex3;
+            }
+            curHex.highlighted = true;
+        }
+
+        private void unhighlightHex(object sender, MouseEventArgs e)
+        {
+            curPol.StrokeThickness /= 2;
+            curHex.highlighted=false;
+            /*
+            THIS curHex = null; is BAAD!! there is no way of telling if this fires AFTER another hex just said
+            curHex = myTestHex.. might unselect it all the time
+            */
+            //curHex = null;
+            //curPol = null;
+        }
+
+        private void selectHex(object sender, MouseButtonEventArgs e)
+        {
+            if(curHex.highlighted)
+            {
+                centerLabel.Text = "Center: " + curHex.center.ToString();
+            }
+            else
+            {
+                centerLabel.Text = "center: ";
+            }
         }
     }
 }
